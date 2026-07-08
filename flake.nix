8c2cbc1b9ce94b6477166f5976b0586ad6550dfd
Sebastian Fleer <s.fleer@famedly.com>
2026-07-08 22:47:22 +0200

    chore(flake): pull famedly-nixos without SSH

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-26.05";
-    famedly-nixos.url = "git+ssh://git@github.com/famedly/famedly-nixos";
+    famedly-nixos.url = "github:famedly/famedly-nixos";
     home-manager = {
       url = "github:nix-community/home-manager?ref=release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
