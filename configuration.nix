7d8fd79140c64ceb953fbd977c74b9ee8a4607ef
Sebastian Fleer <s.fleer@famedly.com>
2026-07-23 11:23:38 +0200

    feat: enable VA-API

--- configuration.nix
+++ configuration.nix
@@ -104,6 +104,22 @@
     powerOnBoot = false;
   };
 
+  # https://nixos.wiki/wiki/Accelerated_Video_Playback
+  nixpkgs.config.packageOverrides = pkgs: {
+    intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+  };
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD (for HD Graphics starting Broadwell (2014) and newer)
+      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      libvdpau-va-gl
+    ];
+  };
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
+  }; # Force intel-media-driver
+
   # Enable fingerprint scanner
   services.fprintd.enable = true;
 
